263861561e317155eabab550e0278d5d4a38d8c8
ashionky <495519020@qq.com>
2024-07-17 15:38:55 +0800

    1.2.4

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="refoss_ha",
-    version="1.2.3",
+    version="1.2.4",
     author="ashionky",
     author_email="495519020@qq.com",
-    license="MIT",
-    url="https://github.com/ashionky/refoss_ha",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+    ],
     packages=find_packages(),
     python_requires=">=3.11",
 )
